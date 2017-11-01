7eaedcac751a067b4a980429dc19fe43e9db11b3
Adrian Muñoz Mudarra <eladrixhd@gmail.com>
2017-11-01 15:34:01 +0100

    Añadido los menus

---
+++ ExtractorFicherosWPF/ExtractorFicherosWPF/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//Añadido
+using System.IO;
+using System.Diagnostics;
+
+namespace ExtractorFicherosWPF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Metodos
+        /// <summary>
+        /// Cierra la aplicación
+        /// </summary>
+        private void CerrarApp()
+        {
+            App.Current.Shutdown();
+        }
+
+        /// <summary>
+        /// Abre la ventana "Acerca de"
+        /// </summary>
+        private void VentanaAcercaDe_Abrir()
+        {
+            VentanaAcercaDe_Principal ventanaAcercaDe = new VentanaAcercaDe_Principal();
+            ventanaAcercaDe.ShowDialog();
+        }
+
+        /// <summary>
+        /// Abre el GitHub de Pablo
+        /// </summary>
+        private void AbrirGit_Pablo()
+        {
+            Process.Start("https://github.com/rasky0607/");
+        }
+
+        /// <summary>
+        /// Abre el GitHub de Adrian
+        /// </summary>
+        private void AbrirGit_Adrian()
+        {
+            Process.Start("https://github.com/ElAdrixHD/");
+        }
+
+        #endregion
+
+        private void MiMenu_Salir_Click(object sender, RoutedEventArgs e)
+        {
+            CerrarApp();
+        }
+
+        private void MiMenu_Acerca_Click(object sender, RoutedEventArgs e)
+        {
+            VentanaAcercaDe_Abrir();
+        }
+
+        private void MiMenu_Git_Pablo_Click(object sender, RoutedEventArgs e)
+        {
+            AbrirGit_Pablo();
+        }
+
+        private void MiMenu_Git_Adrian_Click(object sender, RoutedEventArgs e)
+        {
+            AbrirGit_Adrian();
+        }
+    }
+}
---
+++ ExtractorFicherosWPF/ExtractorFicherosWPF/VentanaAcercaDe_Principal.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ExtractorFicherosWPF
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaAcercaDe_Principal.xaml
+    /// </summary>
+    public partial class VentanaAcercaDe_Principal : Window
+    {
+        public VentanaAcercaDe_Principal()
+        {
+            InitializeComponent();
+        }
+        #region Metodos
+        private void Cerrar_AcercaDe()
+        {
+            Ventana_AcercaDe.Close();
+        }
+        #endregion
+
+        private void Boton_AcercaDe_Cerrar_Click(object sender, RoutedEventArgs e)
+        {
+            Cerrar_AcercaDe();
+        }
+    }
+}
